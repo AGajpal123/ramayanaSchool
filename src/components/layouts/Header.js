a571a78c9231fab8519623f008d9d1cf0bee17ab
Ayushi Gajpal <AGajpal@ind.xebiadigital.com>
2023-09-05 18:40:00 +0530

    ok

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -5,6 +5,9 @@ const Header = () => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <a>
+          <img src='./removeBg.png' height={'50px'} width={'50px'}/>
+        </a>
         <a className="navbar-brand" href="#">Ramayana Foundation School</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
